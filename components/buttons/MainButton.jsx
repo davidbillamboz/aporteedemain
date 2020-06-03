8e4f55af7e3d0d6a43a08b7918c46eb3190eadfe
Njörd <davidbillamboz@gmail.com>
2020-06-04 01:01:11 +0200

    Implement link button

--- components/buttons/MainButton.jsx
+++ components/buttons/MainButton.jsx
@@ -1,27 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import NextLink from 'next/link';
 import { ChevronRight as ChevronRightIcon } from 'react-feather';
 
-const MainButton = ({ title, disabled, ...rest }) => (
-  <button
-    type="button"
-    className={`rounded-full bg-chelseaCucumber ${
-      !disabled ? 'hover:bg-opacity-95' : 'opacity-50 cursor-default'
-    } text-white font-extrabold tracking-wider uppercase text-xl h-20 px-6 leading-none flex items-center shadow`}
-    {...rest}
-  >
-    <ChevronRightIcon className="inline-block mr-2 w-10 h-auto" />
-    <span className="inline-block">{title}</span>
+const Button = ({ children, ...rest }) => (
+  <button type="button" {...rest}>
+    {children}
   </button>
 );
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const Link = ({ children, href, ...rest }) => (
+  <NextLink href={href}>
+    <a {...rest}>{children}</a>
+  </NextLink>
+);
+
+Link.propTypes = {
+  children: PropTypes.node.isRequired,
+  href: PropTypes.string.isRequired,
+};
+
+const MainButton = ({ title, disabled, href, ...rest }) => {
+  const Component = href ? Link : Button;
+
+  return (
+    <Component
+      href={href}
+      className={`rounded-full bg-chelseaCucumber ${
+        !disabled ? 'hover:bg-opacity-95' : 'opacity-50 cursor-default'
+      } text-white font-extrabold tracking-wider uppercase text-xl h-20 px-6 leading-none flex items-center shadow`}
+      {...rest}
+    >
+      <ChevronRightIcon className="inline-block mr-2 w-10 h-auto" />
+      <span className="inline-block">{title}</span>
+    </Component>
+  );
+};
 
 MainButton.propTypes = {
   title: PropTypes.string.isRequired,
   disabled: PropTypes.bool,
+  href: PropTypes.string,
 };
 
 MainButton.defaultProps = {
   disabled: false,
+  href: null,
 };
 
 export default MainButton;
