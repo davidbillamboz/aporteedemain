4609c6a93101a2132663939275ce5b174efb4ed7
Njörd <davidbillamboz@gmail.com>
2020-09-23 11:04:18 +0200

    Fix fontawesome icon bug

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import Layout from '../components/Layout';
 import '../styles/index.css';
 import '../styles/burger.scss';
+
+// Do not auto add Font Awesome css
+config.autoAddCss = false;
 
 const MyApp = ({ Component, pageProps }) => {
   return (
