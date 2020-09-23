cac1a24f83cc02400e931f77d679582be2371131
Njörd <davidbillamboz@gmail.com>
2020-09-23 11:32:40 +0200

    Add google analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Router from 'next/router';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import Layout from '../components/Layout';
@@ -8,6 +9,23 @@ import '../styles/burger.scss';
 
 // Do not auto add Font Awesome css
 config.autoAddCss = false;
+
+// Track page view
+Router.events.on('routeChangeComplete', (url) => {
+  if (!window.gtag) {
+    return;
+  }
+  setTimeout(() => {
+    window.gtag(
+      'config',
+      process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID,
+      {
+        page_location: url,
+        page_title: document.title,
+      }
+    );
+  }, 0);
+});
 
 const MyApp = ({ Component, pageProps }) => {
   return (
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,10 +4,13 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const isProduction = process.env.NODE_ENV === 'production';
+    return { ...initialProps, isProduction };
   }
 
   render() {
+    const { isProduction } = this.props;
+
     return (
       <Html className="bg-white antialiased">
         <Head>
@@ -15,6 +18,32 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Krub:wght@400;600;700&family=Montserrat:wght@400;600;700;800&display=swap"
             rel="stylesheet"
           />
+          {isProduction && (
+            <>
+              {/* Global Site Tag (gtag.js) - Google Analytics */}
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID}`}
+              />
+              <script
+                // eslint-disable-next-line react/no-danger
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('consent', 'default', {
+                      'ad_storage': 'denied',
+                      'analytics_storage': 'denied'
+                    });                    
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                }}
+              />
+            </>
+          )}
         </Head>
         <body className="font-body text-deepKoamaru">
           <Main />
